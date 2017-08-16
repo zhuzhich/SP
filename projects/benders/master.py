83e4035371f110d711fd3d8945d0442396fa2506
zhuzhich <zhich.zhu@gmail.com>
2017-08-16 00:19:03 -0500

    update 4

--- projects/benders/master.py
+++ projects/benders/master.py
@@ -20,41 +20,45 @@ model.sI 		= RangeSet(model.pI)
 model.pT		= Param(default=5)						#time horizon
 model.sT		= range(0,model.pT.value+1)				#time set [0,pT]
 model.ps		= Param(default=2)						#starting time
-model.pR 		= Param(default=model.pTime.value + 1)	#max individuals
+model.pR 		= Param(default=model.pT.value + 1)	#max individuals
 model.sR 		= RangeSet(model.pR)
 model.pCPR		= Param(model.sI)						#PR cost
 model.pCCR		= Param(model.sI)						#CR cost
 model.pKesi		= Param(model.sI)						#failure state 
-
-#second stage variable
-model.nY = Param(within=PositiveIntegers)
-model.sY = RangeSet(model.nY)
+model.pd		= Param(default=5)						#set-up cost
 model.CUTS = Set(within=PositiveIntegers, ordered=True)
 #Subproblem constraint number, for dual solution.
 #dual solution
 model.dSolution = Param(model.Scen, model.CUTS, \
-			default=0.0, mutable=True)
-model.pH = Param(model.Scen)
-model.pT = Param()
+					default=0.0, mutable=True)
+model.pH 		= Param(model.Scen)
+model.pT 		= Param()
 ######################################
 #Variables
 ######################################
-model.x =  Var()
-model.Min_Stage2_cost = Var(model.Scen)
+model.x 		= Var(model.sI,within=Binary)
+model.z 		= Var(within=Binary)
+model.sita 		= Var(model.Scen)
 ######################################
 #Constraints
 ######################################
-def m_c1_rule(model):
-	return model.x<=10 and model.x>=0
-model.c_x_1 = Constraint(rule=m_c1_rule)	
+def m_c1_rule(model,i):
+	return model.x[i] >= model.pKesi[i]
+model.c_x_1 	= Constraint(model.sI, rule=m_c1_rule)	
+def m_c2_rule(model,i):
+	return model.z >= model.x[i]
+model.c_x_2 	= Constraint(model.sI, rule=m_c2_rule)
 
-model.Cut_Defn = ConstraintList()
+model.Cut_Defn 	= ConstraintList()
 
 ######################################
 #Objective
 ######################################
 def min_obj_rule(model):
-	return sum(model.Min_Stage2_cost[i] for i in model.Scen)
-model.oMaster = Objective(rule=min_obj_rule,sense=minimize)
+	return model.pd*model.z+\
+		   sum(model.pCPR*model.x[i] for i in model.sI)+\
+		   sum((model.pCCR-model.pCPR)*model.pKesi[i] for i in model.sI)+\
+		   sum(model.sita[i] for i in model.Scen)
+model.oMaster 	= Objective(rule=min_obj_rule,sense=minimize)
 	
 	
--- projects/benders/sub.py
+++ projects/benders/sub.py
@@ -1,7 +1,7 @@
-#  Implement Bender's decomposition in Birge book
-#  Chapter 5, example 1, pp184
+#  Implemention of bender's decomposition of SORP considering PM
 #  Author: Zhicheng Zhu
 #  Email: zzhu3@lamar.edu
+
 
 #Subproblem
 ##
@@ -21,30 +21,100 @@ model.dual = Suffix(direction=Suffix.IMPORT)
 #####################################
 #Parameters
 #####################################
-# first stage variable
-#model.nX = Param(within=PositiveIntegers)
-#model.sX = RangeSet(1,model.nX)
-#second stage variable
-model.nY = Param(within=PositiveIntegers)
-model.sY = RangeSet(1,model.nY)
+model.NUMSCEN 	= Param(default=4)						#scenarios
+model.Scen		= RangeSet(model.NUMSCEN)				
+model.prob 		= Param(default=1.0/model.NUMSCEN.value)#equal probability
+model.pI		= Param(default=4)       				#component numbers
+model.sI 		= RangeSet(model.pI)
+model.pT		= Param(default=5)						#time horizon
+model.sT		= range(0,model.pT.value+1)				#time set [0,pT]
+model.sT_End	= model.sT[:-1]							#sT-end = [0,end-1]
+model.sT_0		= model.sT[1:]							#sT-0=[1,end]
+model.ps		= Param(default=2)						#starting time
+model.pR 		= Param(default=model.pT.value + 1)		#max individuals
+model.sR 		= range(1,model.pR.value)
+model.sR_End	= model.sR[:-1]
+model.sR_0		= model.sR[1:]	
+model.pCPR		= Param(model.sI)						#PR cost
+model.pCCR		= Param(model.sI)						#CR cost
+model.pKesi		= Param(model.sI)						#failure state 
+model.pd		= Param(default=5)						#set-up cost
 
-#kesi
-model.pKesi = Param(within=PositiveIntegers)
-
-#first stage
-model.x = Param(within=NonNegativeReals, mutable=True)
+#first stage variable
+model.x			= Param(model.pI, within=NonNegativeReals, mutable=True)
 
 ######################################
 #Variables
 ######################################
-model.y =  Var(model.sY, within=NonNegativeReals)
+model.xs		= Var(model.sI, model.sT, model.sR, within=Binary)
+model.ws		= Var(model.sI, model.sT, model.sR, within=Binary)
+model.zs		= Var(model.sT_0, within=Binary)
+model.u			= Var(model.sI, model.sT, model.sR, within=Binary)
+model.v			= Var(model.sI, model.sT, model.sR, within=Binary)
 
 ######################################
 #Constraints
 ######################################
-def s_c1_rule(model):
-	return model.y[model.sY[1]]-model.y[model.sY[2]] == model.pKesi-model.x
-model.cY1 = Constraint(rule=s_c1_rule)
+def s_cb_rule(model, i, t, r):
+	return model.xs[i,t,r]<=model.xs[i,t+1,r]
+model.cSb = Constraint(model.sI, model.sT_End, model.sR, rule=s_cb_rule)
+
+def s_cc_rule(model, i, t, r):
+	return model.xs[i,t+1,r+1]<=model.xs[i,t,r]
+model.cSc = Constraint(model.sI, model.sT_End, model.sR_End, rule=s_cc_rule)
+
+def s_cd_rule(model, i, t):
+	return sum(model.xs[i,t,r]-model.xs[i,t-1,r] for r in model.sR)<=model.zs[t]
+model.cSd = Constraint(model.sI, model.sT_0, rule=s_cd_rule)
+
+def s_cf_rule(model, i):
+	return model.xs[i,0,r]<=model.zs[0]
+model.cSf = Constraint(model.sI, rule=s_cf_rule)
+####
+def s_cg_rule(model, i, t):## not finish
+	return model.xs[i,t+1,r+1]<=model.xs[i,t,r]
+model.cS2 = Constraint(model.sI, model.sT_End, model.sR_End, rule=s_cg_rule)
+###
+def s_ch_rule(model, i, r):
+	return model.xs[i,0,r]=0
+model.cSh = Constraint(model.sI, model.sR_0, rule=s_ch_rule)
+
+def s_ci_rule(model, i):
+	return mode.x[i]=model.xs[i,0,1]
+model.cid = Constraint(model.sI, rule=s_ci_rule)
+
+#def s_cj_rule(model, i, t):
+#	return sum(model.xs[i,t,r]-model.xs[i,t-1,r] for r in model.sR)<=model.zs[t]
+#model.cSj = Constraint(model.sI, model.sT_0, rule=s_cj_rule)
+
+def s_ck_rule(model, i, t, r):
+	return model.ws[i,t,r] = model.xs[i,t,r]-model.xs[i,t-1,r]
+model.cSk = Constraint(model.sI, model.sT_0, model.sR, rule=s_ck_rule)
+
+def s_cl_rule(model, i, r):
+	return model.ws[i,0,r]=model.xs[i,0,r]
+model.cSl = Constraint(model.sI, model.sR, rule=s_cl_rule)
+
+def s_cm_rule(model, i, r, t):#not finish
+	return smodel.ws[i,t,r]=model.ws[i,t-model.pT.value,r]
+model.cSm = Constraint(model.sI, model.sR, model.sT, rule=s_cm_rule)
+
+def s_cn_rule(model, i, r, t):#not finish
+	return model.ws[i,t,r]=0
+model.cSn = Constraint(model.sI, model.sR, model.sT_0, rule=s_cn_rule)
+
+def s_co_rule(model, i, t):#not finish
+	return 
+model.cSo = Constraint(model.sI, model.sT_0, rule=s_co_rule)
+
+def s_cp_rule(model, i, t):#not finish
+	return sum(model.xs[i,t,r]-model.xs[i,t-1,r] for r in model.sR)<=model.zs[t]
+model.cSp = Constraint(model.sI, model.sT_0, rule=s_cp_rule)
+
+def s_cq_rule(model, i, t):
+	return model.u[i,t,r]+model.v[i,t,r]<=1
+model.cSq = Constraint(model.sI, model.sT, model.sR, rule=s_cq_rule)
+
 
 ######################################
 #Objective
