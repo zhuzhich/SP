83ec50f9034b8a870bd073b24fe22fd7cbd893d6
zhicheng zhu <zhicheng.zhu@ttu.edu>
2019-05-27 16:00:43 -0500

    1. new files, rolling DEF

---
+++ projects/rolling_horizon/DEF_roll/ef.py
@@ -0,0 +1,297 @@
+#  Author: Zhicheng Zhu
+#  Email: zzhu3@lamar.edu
+
+#  Implemention of extensive form. Whole problem.
+#
+#Last Update: 03/21/2018
+#
+
+from pyomo.core import *
+import math
+import random
+
+model = AbstractModel(name="subProblem")
+
+#
+model.rc = Suffix(direction=Suffix.IMPORT)
+model.lrc = Suffix(direction=Suffix.IMPORT)
+model.urc = Suffix(direction=Suffix.IMPORT)
+model.dual = Suffix(direction=Suffix.IMPORT)
+
+
+#####################################
+#Parameters
+#####################################
+model.NUMSCEN 	= Param()								#scenarios
+model.Scen		= RangeSet(model.NUMSCEN)				
+model.prob 		= Param(default=1.0/model.NUMSCEN)		#equal probability
+model.pI		= Param()       						#component numbers
+model.sI 		= RangeSet(model.pI)
+model.pT		= Param()								#time horizon
+model.pT_1		= Param()								#
+model.sT		= RangeSet(0,model.pT)					#time set [0,pT]
+model.sT_End	= RangeSet(0,model.pT-1)				#sT_end = [0,end-1]
+model.sT_0		= RangeSet(1,model.pT)					#sT_0=[1,end]
+model.pT_ex		= Param()								#Extended time horizon
+model.sT_ex		= RangeSet(0,model.pT_ex)				#extended time set [0,pT_ex]
+model.sT_exT	= RangeSet(model.pT+1, model.pT_ex)		#set[pT+1, pT_ex]
+model.pR 		= Param()								#max individuals
+model.sR 		= RangeSet(1,model.pR)
+model.sR_End	= RangeSet(1,model.pR-1)
+model.sR_0		= RangeSet(2,model.pR)
+model.pd		= Param(default=5)						#set-up cost
+model.randSeed  = Param()
+
+model.ps 		= Param(model.sI)
+model.pKesi 	= Param(model.sI)
+model.pCCR 		= Param(model.sI)
+model.pCPR 		= Param(model.sI)
+model.w_shape 	= Param(model.sI)
+model.w_scale 	= Param(model.sI)
+
+'''
+model.ss		= Set()								#age
+model.sKesi		= Set()								#failure state
+model.sCCR		= Set()
+model.sCPR		= Set()
+model.s_w_shape	= Set()
+model.s_w_scale	= Set()
+
+
+#For the large number of components
+#generate parameters.
+
+#PR cost
+res = [];
+for i in model.ss:
+	res.append(i);
+def ps_init(model, i):
+	return res[i-1];
+model.ps = Param(model.sI, initialize=ps_init);
+
+res = [];
+for i in model.sCPR:
+	res.append(i);
+def pCPR_init(model, i):
+	return res[i-1];
+model.pCPR		= Param(model.sI, initialize=pCPR_init)  #PR cost	
+
+
+res = [];
+for i in model.sCCR:
+	res.append(i);
+#CR cost
+def pCCR_init(model, i):
+	return res[i-1];			
+model.pCCR		= Param(model.sI, initialize=pCCR_init)	 #CR cost
+
+res = [];
+for i in model.sKesi:
+	res.append(i);
+#kesi
+def pKesi_init(model, i):
+	return res[i-1];
+model.pKesi		= Param(model.sI, initialize=pKesi_init)  #failure state
+
+res = [];
+for i in model.s_w_shape:
+	res.append(i);
+#weibull shape parameter
+def w_shape_init(model, i):
+	return res[i-1];
+model.w_shape		= Param(model.sI, initialize=w_shape_init) #weibull shape
+
+res = [];
+for i in model.s_w_scale:
+	res.append(i);
+#weibull scale parameter
+def w_scale_init(model, i):
+	return res[i-1];
+model.w_scale		= Param(model.sI, initialize=w_scale_init) #weibull scale
+'''
+
+#random life time
+def pLT_init(model, i, r, w):
+	random.seed(i+r+w+model.randSeed)
+	if r == 1:
+		if model.pKesi[i]==1:
+			return 0
+	#return i #for test only..
+		else:
+			ran_num = random.uniform(0,1)
+			ran_num_log = -math.log(ran_num)
+			s_inv = 1.0/model.w_shape[i]	
+			LT1 = int((ran_num_log**s_inv)*model.w_scale[i]) - model.ps[i]					
+			LT = max(1,LT1)
+			return LT
+	else:	
+		ran_num = random.uniform(0,1)
+		ran_num_log = -math.log(ran_num)
+		s_inv = 1.0/model.w_shape[i]	
+		LT1 = int((ran_num_log**s_inv)*model.w_scale[i])				
+		LT = max(1,LT1)
+		return max(1,LT)
+model.pLT		= Param(model.sI, model.sR, model.Scen, initialize=pLT_init)
+
+#set of constraint f
+def scf_init(model):
+	return ((i,r,t,w) for i in model.sI for r in model.sR_End for t in model.sT\
+			for w in model.Scen if t<=model.pT-model.pLT[i,r+1,w])
+model.scf		= Set(dimen=4, initialize=scf_init)
+
+#set of constraint g
+def scg_init(model):
+	return ( (i,w) for i in model.sI for w in model.Scen if model.pLT[i,1,w]<=model.pT)
+model.scg		= Set(dimen=2, initialize=scg_init)
+
+#set of constraint m
+def scm_init(model):
+	return ((i,r,t,w) for i in model.sI for r in model.sR for t in model.sT_exT\
+			for w in model.Scen if t<=model.pT+model.pLT[i,r,w]-1)
+model.scm		= Set(dimen=4, initialize=scm_init)
+
+#set of constraint n
+def scn_init(model):
+	return ((i,r,t,w) for i in model.sI for r in model.sR for t in model.sT_exT\
+			for w in model.Scen if t>=model.pT+model.pLT[i,r,w])
+model.scn		= Set(dimen=4, initialize=scn_init)
+
+######################################
+#Variables
+######################################
+if 1:
+#integer
+	model.x 		= Var(model.sI, within=Binary)
+	model.xs		= Var(model.sI, model.sT, model.sR, model.Scen, within=Binary)
+	model.ws		= Var(model.sI, model.sT_ex, model.sR, model.Scen, within=Binary)
+	model.zs		= Var(model.sT, model.Scen, within=Binary)
+	model.u			= Var(model.sI, model.sT, model.sR, model.Scen, within=Binary)
+	model.v			= Var(model.sI, model.sT, model.sR, model.Scen, within=Binary)
+else:
+#relax
+	model.x 		= Var(model.sI, within=NonNegativeReals)
+	model.xs		= Var(model.sI, model.sT, model.sR, model.Scen, within=NonNegativeReals)
+	model.ws		= Var(model.sI, model.sT_ex, model.sR, model.Scen, within=NonNegativeReals)
+	model.zs		= Var(model.sT, model.Scen, within=NonNegativeReals)
+	model.u			= Var(model.sI, model.sT, model.sR, model.Scen, within=NonNegativeReals)
+	model.v			= Var(model.sI, model.sT, model.sR, model.Scen, within=NonNegativeReals)
+#model.test = Var(bounds=(0,0.5),initialize=1)
+######################################
+#Constraints
+######################################
+
+def s_cb_rule(model, i, t, r, w):
+	return model.xs[i,t,r,w]<=model.xs[i,t+1,r,w]
+model.cSb = Constraint(model.sI, model.sT_End, model.sR, model.Scen, rule=s_cb_rule)
+
+def s_cc_rule(model, i, t, r, w):
+	return model.xs[i,t+1,r+1,w]<=model.xs[i,t,r,w]
+model.cSc = Constraint(model.sI, model.sT_End, model.sR_End, model.Scen, rule=s_cc_rule)
+
+def s_cd_rule(model, i, t, w):
+	#return sum(model.xs[i,t,r]-model.xs[i,t-1,r] for r in model.sR)<=model.zs[t]
+	return sum(model.ws[i,t,r,w] for r in model.sR)<=model.zs[t,w]
+model.cSd = Constraint(model.sI, model.sT_0, model.Scen, rule=s_cd_rule)
+
+def s_ce_rule(model, i, w):
+	return model.xs[i,0,1,w]<=model.zs[0,w]
+model.cSe = Constraint(model.sI,model.Scen, rule=s_ce_rule)
+
+#def s_ce1_rule(model):
+#	return model.z == model.zs[0]
+#model.cSe1 = Constraint(rule=s_ce1_rule)
+
+def s_cf_rule(model, i, r, t,w):
+	return model.xs[i,t,r,w]<=model.xs[i,t+model.pLT[i,r+1,w],r+1,w]
+model.cSf = Constraint(model.scf, rule=s_cf_rule)
+
+def s_cg_rule(model, i, w):
+	return model.xs[i,model.pLT[i,1,w],1,w]==1
+model.cSg = Constraint(model.scg, rule=s_cg_rule)
+
+def s_ch_rule(model, i, r, w):
+	return model.xs[i,0,r,w]==0
+model.cSh = Constraint(model.sI, model.sR_0, model.Scen, rule=s_ch_rule)
+
+def s_ci_rule(model, i, w):
+	return model.xs[i,0,1,w]==model.x[i]
+model.cSi = Constraint(model.sI, model.Scen, rule=s_ci_rule)
+
+def s_cj_rule(model, i):
+	return model.x[i] >= model.pKesi[i]
+model.cSj = Constraint(model.sI, rule=s_cj_rule)
+
+def s_ck_rule(model, i, t, r, w):
+	return model.ws[i,t,r,w] == model.xs[i,t,r,w]-model.xs[i,t-1,r,w]
+model.cSk = Constraint(model.sI, model.sT_0, model.sR, model.Scen, rule=s_ck_rule)
+
+def s_cl_rule(model, i, r, w):
+	return model.ws[i,0,r,w]==model.xs[i,0,r,w]
+model.cSl = Constraint(model.sI, model.sR, model.Scen, rule=s_cl_rule)
+
+def s_cm_rule(model, i, r, t, w):
+	return model.ws[i,t,r,w]==model.ws[i,t-model.pT.value,r,w]
+model.cSm = Constraint(model.scm, rule=s_cm_rule)
+
+def s_cn_rule(model, i, r, t, w):
+	return model.ws[i,t,r,w]==0
+model.cSn = Constraint(model.scn, rule=s_cn_rule)
+
+def s_co_rule(model, i, t, r, w):
+	return model.u[i,t,r,w]-model.v[i,t,r,w]==\
+			model.ws[i,t+model.pLT[i,r,w],r,w]-model.ws[i,t,r-1,w]
+model.cSo = Constraint(model.sI, model.sT, model.sR_0, model.Scen, rule=s_co_rule)
+
+def s_cp_rule(model, i, t, w):
+	return model.u[i,t,1,w]-model.v[i,t,1,w]==\
+			model.ws[i,t+model.pLT[i,1,w],1,w]-1
+model.cSp = Constraint(model.sI, model.sT, model.Scen, rule=s_cp_rule)
+
+def s_cq_rule(model, i, t, r, w):
+	return model.u[i,t,r,w]+model.v[i,t,r,w]<=1
+model.cSq = Constraint(model.sI, model.sT, model.sR, model.Scen, rule=s_cq_rule)
+
+def s_cr_rule(model, i, t, r, w):
+	return model.u[i,t,r,w]<=1 #and model.u[i,t,r]>=0
+model.cSr = Constraint(model.sI, model.sT, model.sR, model.Scen, rule=s_cr_rule)
+
+def s_cs_rule(model, i, t, r, w):
+	return model.v[i,t,r,w]<=1 #and model.v[i,t,r]>=0
+model.cSs = Constraint(model.sI, model.sT, model.sR, model.Scen, rule=s_cs_rule)
+
+def s_cs1_rule(model, i, r, w):
+	return sum(model.u[i,t,r,w]+model.v[i,t,r,w] for t in model.sT) <= 2
+model.cSs1 = Constraint(model.sI, model.sR_0, model.Scen, rule=s_cs1_rule)
+
+def s_ct_rule(model, i, t, r, w):
+	return model.xs[i,t,r,w]<=1 #and model.xs[i,t,r]>=0
+model.cSt = Constraint(model.sI, model.sT, model.sR, model.Scen, rule=s_ct_rule)
+
+def s_cu_rule(model, t, w):
+	return model.zs[t, w]<=1 #and model.zs[t]>=0
+model.cSu = Constraint(model.sT, model.Scen, rule=s_cu_rule)
+
+def s_cv_rule(model, i, t, r, w):
+	return model.ws[i,t,r,w]<=1 #and model.ws[i,t,r]>=0
+model.cSv = Constraint(model.sI, model.sT_ex, model.sR, model.Scen, rule=s_cv_rule)
+
+def s_cw_rule(model, i):
+	return model.x[i]<=1 
+model.cSw = Constraint(model.sI, rule=s_cw_rule)
+"""
+"""
+######################################
+#Objective
+######################################
+def exp_cost_rule(model):
+	model.idr1   = sum(model.pCCR[i]*model.ws[i,model.pLT[i,1,w],1,w]+\
+			 model.pCPR[i]*(1-model.ws[i,model.pLT[i,1,w],1,w])\
+			 for i in model.sI for w in model.Scen)
+	model.othIdr = sum(sum(model.pCCR[i]*(1-0.5*sum(model.u[i,t,r,w]+model.v[i,t,r,w] for t in model.sT))-\
+					model.pCCR[i]*(1-0.5*(model.xs[i,model.pT,r,w]+model.xs[i,model.pT,r-1,w]))+\
+					model.pCPR[i]*0.5*sum(model.u[i,t1,r,w]+model.v[i,t1,r,w] for t1 in model.sT)\
+					for r in model.sR_0)-0.5*model.pCPR[i]\
+					for i in model.sI for w in model.Scen)
+	model.setup  = sum(model.pd*model.zs[t,w] for t in model.sT for w in model.Scen)
+	return (model.idr1+model.othIdr+model.setup)/model.NUMSCEN
+model.objCost = Objective(rule=exp_cost_rule,sense=minimize)
---
+++ projects/rolling_horizon/DEF_roll/run.py
@@ -0,0 +1,261 @@
+# Author Zhicheng Zhu
+# Email: zzhu3@lamar.edu
+
+# solve extensive form model using Pyomo
+# 
+# Structure:
+# run.py: main script file
+# ef.py:  extensive form model
+# ef.dat: extensive form data.
+
+#
+#Last Update: 03/21/2018
+#
+from pyutilib.misc import import_file
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+from pyomo.opt.base import SolverFactory
+from pyomo.opt.parallel import SolverManagerFactory
+from pyomo.opt.parallel.manager import solve_all_instances
+import time
+import math
+import random
+import numpy as np
+class system_info():
+	def weibull_cdf(self, i, t):
+		tmp = float(t)/self.w_scale[i];
+		tmp1 = tmp ** self.w_shape[i];
+		return 1 - math.exp(-tmp1);
+		
+	def cond_fail_prob(self, i, ageFrom, ageTo):
+		if ageTo - ageFrom != 1:
+			prob = 0;
+		else:
+			tmp = self.weibull_cdf(i, ageTo) -  self.weibull_cdf(i, ageFrom);
+			tmp1 = 1 -  self.weibull_cdf(i, ageFrom);
+			prob = float(tmp)/tmp1;	
+		return prob;
+
+
+	def __init__(self, N, T, inspInterval, cS, age,\
+				failState, cPR, cCR, w_shape, w_scale):
+		self.nComponents = N;
+		self.nStages = T;
+		self.inspItvl = inspInterval;
+		self.cS = cS;
+		self.age = age;
+		self.failState = failState;
+		self.cPR = cPR;
+		self.cCR = cCR;
+		self.cS = cS;
+		self.w_shape = w_shape;
+		self.w_scale = w_scale;
+
+		#output
+
+
+def create_files(randSeed, sysInfo):
+
+	file_name = "C:\\Users\\Yisha\\Desktop\\ZZC\\DEF_roll\\ef.dat"
+	f = open(file_name,"w")
+
+
+	W = 50;
+	T_ex = 50;
+
+	f.write("param NUMSCEN := " + str(W)+";\n\n");
+	
+	f.write("param prob := " + str(1.0/W)+";\n\n")	;
+	
+	f.write("param pI := " + str(sysInfo.nComponents)+";\n\n");
+
+	f.write("param pT := " + str(sysInfo.nStages)+";\n\n");
+
+	f.write("param pT_ex := " + str(T_ex)+";\n\n");
+	
+	f.write("param pd := " + str(sysInfo.cS)+";\n\n");
+
+	f.write("param pR := " + str(sysInfo.nStages+2)+";\n\n")
+	
+	f.write("param randSeed := " + str(randSeed)+";\n\n")
+
+	text1 = "param ps := ";
+	text2 = "param pKesi := ";
+	text3 = "param pCCR := ";
+	text4 = "param pCPR := ";
+	text5 = "param w_shape := ";
+	text6 = "param w_scale := ";
+	for i in range(sysInfo.nComponents):
+		text1 = text1 + str(i+1) + " " + str(sysInfo.age[i]) + " ";
+		text2 = text2 + str(i+1) + " " + str(sysInfo.failState[i]) + " ";
+		text3 = text3 + str(i+1) + " " + str(sysInfo.cCR[i]) + " ";
+		text4 = text4 + str(i+1) + " " + str(sysInfo.cPR[i]) + " ";
+		text5 = text5 + str(i+1) + " " + str(sysInfo.w_shape[i]) + " ";
+		text6 = text6 + str(i+1) + " " + str(sysInfo.w_scale[i]) + " ";
+	
+	text1 = text1 + ";\n\n";
+	text2 = text2 + ";\n\n";
+	text3 = text3 + ";\n\n";
+	text4 = text4 + ";\n\n";
+	text5 = text5 + ";\n\n";
+	text6 = text6 + ";\n\n";
+	
+	f.write(text1);
+	f.write(text2);	
+	f.write(text3);	
+	f.write(text4);
+	f.write(text5);
+	f.write(text6);	
+	'''
+## create lifetimes
+	for idx in range(0,w_Lo):
+		file_name = dir_local + "\\nodedata\\ScenNode"+str(idx+1)+".dat"
+		f = open(file_name,"w")
+		f.write("#pLT is two-dimensional [i,r]=[component, individual]\n#[i,*] means all data in row i.\n\n")
+		f.write("param pLT := ")
+		for idx1 in range(0,I_Lo):
+			i = idx1 + 1
+			f.write("\n["+str(i)+",*]\n")
+			for idx2 in range(0,R_Lo):
+				r = idx2 + 1
+				random.seed(i+r+idx+1) ###control the seed
+				if r == 1:
+					if pKesi_Lo[idx1] == 1:
+						LT = 0
+					else:    
+						ran_num = random.uniform(0,1)
+						ran_num_log = -math.log(ran_num)
+						s_inv = 1.0/w_shape_Lo[idx1]	
+						LT1 = int((ran_num_log**s_inv)*w_scale_Lo[idx1]) - s_Lo					
+						LT = max(1,LT1)
+				else:
+					ran_num = random.uniform(0,1)
+					ran_num_log = -math.log(ran_num)
+					s_inv = 1.0/w_shape_Lo[idx1]
+					LT1 = int((ran_num_log**s_inv)*w_scale_Lo[idx1])
+					LT = max(1,LT1)
+				f.write(str(r)+ " " + " "*(2-len(str(r))) + str(LT)+ "    ")
+				#f.write(str(r)+ " " + str(LT)+ "   ")
+				if r%10 == 0:
+					f.write("\n")
+			f.write("\n")
+		f.write(";")
+		f.close()
+'''
+	f.close();
+
+##
+#start from here!
+nComponents = 3;
+nStages = 7;
+inspInterval = 1;
+cS = 5;
+
+failState = [0]*nComponents;
+age = [0]*nComponents;
+cPR = [0]*nComponents;
+cCR = [0]*nComponents;
+w_shape = [0]*nComponents;
+w_scale = [0]*nComponents;
+
+for i in range(nComponents):
+	#kesi
+	if i==0:
+		failState[i] = 1;
+	#age
+	age[i] = 2;
+	#cPR
+	cPR[i] = 1;
+	#cCR
+	random.seed(i*30);
+	temp = random.uniform(6,16);
+	cCR[i] = round(temp,1);			
+	#shape
+	random.seed(i*20)   
+	temp = random.uniform(4,7)
+	w_shape[i] = round(temp,1);			
+	#scale
+	random.seed(i*10)   
+	temp = random.uniform(1,8)#(4,11)
+	w_scale[i] = round(temp,1);		
+
+
+
+sysInfo = system_info(nComponents, nStages, inspInterval, cS, age,\
+				failState, cPR, cCR, w_shape, w_scale);
+
+
+rep = 1;
+cost = []	
+
+for randSeed in range(rep):	
+	cost_rep = 0;
+	for t in range(1):
+		if t == nStages - 1:
+			tmp = 0;
+			for i in range(sysInfo.nComponents):
+				tmp += sysInfo.cCR[i] * sysInfo.failState[i];
+			if tmp > 0:
+				tmp += sysInfo.cS;
+			cost_rep += tmp;
+		else:
+			create_files(randSeed, sysInfo);
+		
+			# initialize the instances.
+			ef_mdl = import_file("ef.py").model
+			ef_insts=[]
+			ef_insts.append(ef_mdl.create_instance(name="ef_instance", filename="ef.dat"))
+			solver_manager = SolverManagerFactory("serial")
+			#start_time1 = time.clock()
+			solve_all_instances(solver_manager, 'cplex', ef_insts);
+			# get first stage solution
+			x = [];
+			tmp = 0;
+			for i in ef_insts[0].sI:
+				x.append(round(ef_insts[0].x[i](),0));
+				tmp += sysInfo.cPR[i-1]*x[i-1];
+				tmp += (sysInfo.cCR[i-1] - sysInfo.cPR[i-1])*sysInfo.failState[i-1];
+			if tmp > 0:
+				tmp += sysInfo.cS;
+			cost_rep += tmp;
+			#prepare next stage:
+			age = [sysInfo.age[i]*(x[i]) for i in range(sysInfo.nComponents)];	#age after maintenance
+			failProb = [sysInfo.cond_fail_prob(i, age[i], age[i]+1) for i in range(sysInfo.nComponents)];
+
+			randProb = [random.uniform(0,1) for i in range(sysInfo.nComponents)];
+			failState = [int(randProb[i]<failProb[i]) for i in range(sysInfo.nComponents)];
+			#print (kesi);
+			for i in range(sysInfo.nComponents):
+				sysInfo.failState[i] = failState[i];	
+				sysInfo.age[i] = age[i] + 1;
+				sysInfo.nStages = nStages - t - 1;
+	cost.append(cost_rep);
+print ("cost");
+print (cost);
+print ("mean cost");
+print (np.mean(cost));
+print ("variance cost");
+print (np.var(cost));	
+print ("objective value=%f" %ef_insts[0].objCost())
+	
+print ("====debug===");
+#print (ef_insts[0].w_shape());	
+for i in ef_insts[0].sI:
+	print("w_shape["+str(i)+"]="+str(round(ef_insts[0].w_shape[i], 4)))	
+	print("x["+str(i)+"]="+str(round(ef_insts[0].x[i](), 4)))	
+			
+			
+			
+#end_time1 = time.clock()
+#print ("solving time = ",str(end_time1-start_time1))
+"""
+for i in ef_insts[0].sI:
+	for t in ef_insts[0].sT:
+		print ("")
+		for r in ef_insts[0].sR:
+			print ("(%d,%d,%d)=(%d,%d)" %(i,t,r,ef_insts[0].u[i,t,r,1](),ef_insts[0].v[i,t,r,1]())),
+for i in ef_insts[0].sI:
+	print("x["+str(i)+"]="+str(round(ef_insts[0].x[i](), 4)))
+print ("objective value=%f" %ef_insts[0].objCost())
+"""
+
